59c4a9cbfa84f92e8084eb008c4841b755cdd7f7
educalok <eduardo.calzone@gmail.com>
2022-11-06 22:11:21 -0300

    CardContainer Added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { Heading, Image, Img, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-function Card () {
-  return <div>Card</div>
+function Card ({ item }) {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  return (
+    <Stack
+      shadow='lg'
+      borderWidth='2px'
+      width={400}
+    >
+      <Image src={item.promo_items.basic.url} />
+      <Stack>
+        <Heading fontSize={'2xl'} fontFamily={'body'} key={uuidv4()}>
+          {item.headlines.basic}
+        </Heading>
+        <Text as='sub' key={uuidv4()}>
+          {new Date(item.display_date).toLocaleDateString('es-ES', options)}
+        </Text>
+      </Stack>
+    </Stack>
+  );
 }
 
-export default Card
+export default Card;
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import { SimpleGrid } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import Card from './Card';
 
 function CardContainer () {
-  return <Card />;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData () {
+    const fetchData = await fetch(
+      'https://lanacion-challenge-nine.vercel.app/lanacion.json'
+    );
+    const results = await fetchData.json();
+    const filteredData = results.articles.filter(
+      article => article.subtype === '7'
+    );
+    console.log(filteredData);
+    setData(filteredData);
+  }
+  return (
+    <SimpleGrid columns={3} spacing={5}>
+      {data &&
+        data.map(item => {
+          return <Card key={uuidv4() + 'card'} item={item} />;
+        })}
+    </SimpleGrid>
+  );
 }
 
 export default CardContainer;
